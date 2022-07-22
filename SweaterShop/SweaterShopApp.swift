f1b56ea66160fd2535e2c76a0e8df6c2bca66293
yongvuthivann <vuthivann.yong007@gmail.com>
2022-07-23 00:46:59 +0700

    Initial Commit

---
+++ SweaterShop/SweaterShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweaterShopApp.swift
+//  SweaterShop
+//
+//  Created by Yong vuthivann on 23/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SweaterShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
